2d0fc1bb980a44994b96acbd49f6b36d21497890
wilkor <wilkorguitarrista@msn.com>
2020-07-25 13:36:01 -0300

    removendo o path do config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,6 @@
 module.exports =  {
 
   url:'https://chat.blip.ai/?appKey=d2lsa29ycmliZWlyb2RlYWxtZWlkYTpkMWYyY2NmYy02MTNiLTRhZjAtYTM0Yi0xMjk1MzUzOWI4ZWM=',
-  path: 'screenshot/',
   startButton: true
 }
 
